acb6f172dcc3214ac05ddc39cabce373b042bc05
kirkandri <kirk.andri@gmail.com>
2021-06-06 18:36:31 +0300

    check buttons pinouts

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,43 @@
 #include <RF24.h>
 #include <FastLED.h>
 
-// #define PIN_C1_BUTTONS A0
-// #define PIN_C2_BUTTONS A1
-// #define PIN_R1_BUTTONS 2
-// #define PIN_R2_BUTTONS 3
-// #define PIN_R3_BUTTONS 4
-// #define PIN_R4_BUTTONS 5
-// #define PIN_R5_BUTTONS 7
+#define DEBUG
+
+#define PIN_C1_BUTTONS 8
+#define PIN_C2_BUTTONS 9
+#define PIN_R1_BUTTONS 7
+#define PIN_R2_BUTTONS 6
+#define PIN_R3_BUTTONS 5
+#define PIN_R4_BUTTONS 4
+#define PIN_R5_BUTTONS 3
 #define PIN_LED        2
 #define NUM_LEDS       8
+#define RADIO_ADDRESS  "1N"
 
-// int pinC_button [] = {A0, A1};
-// int pinR_button [] = {2, 3, 4, 5, 7};
-// bool buttons [2][5] = {0};
+int pinC_button [] = {PIN_C1_BUTTONS, PIN_C2_BUTTONS};
+int pinR_button [] = {PIN_R1_BUTTONS, PIN_R2_BUTTONS, PIN_R3_BUTTONS, PIN_R4_BUTTONS, PIN_R5_BUTTONS};
+bool buttons [2][5] = {0};
 
-// RF24 radio(9, 10); // CE and CSN
-// const byte rxAddr[2] = "1N";  // адрес
+RF24 radio(9, 10); // CE and CSN
+const byte rxAddr[2] = "1N";;  // адрес
 
-// byte counter ;
+byte counter ;
 CRGBArray<NUM_LEDS> leds;
 
 void setup() {
+  #ifdef DEBUG
   Serial.begin (115200);
+  Serial.println("Start");
+  #endif
   FastLED.addLeds<WS2812B, PIN_LED, GRB>(leds, NUM_LEDS);
-  // for (int i = 0; i < 2; i++) {
-  //   pinMode (pinC_button[i], OUTPUT);
-  // }
-  // for (int i = 0; i < 5; i++) {
-  //   pinMode (pinR_button[i], INPUT_PULLUP);
-  // }
-  // digitalWrite(PIN_C2_BUTTONS, LOW);
-  // digitalWrite(PIN_C1_BUTTONS, HIGH);
+  for (int i = 0; i < 2; i++) {
+    pinMode (pinC_button[i], OUTPUT);
+  }
+  for (int i = 0; i < 5; i++) {
+    pinMode (pinR_button[i], INPUT_PULLUP);
+  }
+  digitalWrite(PIN_C2_BUTTONS, LOW);
+  digitalWrite(PIN_C1_BUTTONS, HIGH);
   // radio.begin();
   // radio.setRetries(0, 1);    //(время между попыткой достучаться, число попыток)
   // radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
@@ -48,59 +54,71 @@ void setup() {
   // //должна быть одинакова на приёмнике и передатчике!
   // //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
 
-  // //не слушаем радиоэфир, мы передатчик
-
-  Serial.println("Start");
 }
 
-// void Check_Button() {
-//   for (int i = 0; i < 2; i++) {
-//     digitalWrite(pinC_button[i], LOW);
-//     for (int j = 0; j < 5; j++) {
-//       if (digitalRead(pinR_button[j]) == LOW) {
-//         Serial.print("Get ");
-//         Serial.print(i);
-//         Serial.print(" ");
-//         Serial.print(j);
-//         Serial.println();
-//         counter = i * 5 + j;
-//         radio.write(&counter, sizeof(counter));
-//         delay(100);
-//       }
-//     }
-//     digitalWrite(pinC_button[i], HIGH);
-//   }
-// }
+void Send_radio (byte id){
+  radio.write(&id, sizeof(id));
+}
+void led_blink (byte id){
+  int led_id = 0;
+  Serial.println(id);
+
+  if (!id%5){
+    led_id = (id*2)-10;
+  } else {
+    led_id = (id*2)+1;
+  }
+  Serial.println(led_id);
+
+  leds[led_id] = CRGB(255,255,0);
+  FastLED.show();
+  delay(100);
+  leds[led_id] = CRGB::Black;
+  FastLED.show();
+}
+
+void Check_Button() {
+  for (int i = 0; i < 2; i++) {
+    digitalWrite(pinC_button[i], LOW);
+    for (int j = 0; j < 5; j++) {
+      if (digitalRead(pinR_button[j]) == LOW) {
+        #ifdef DEBUG
+        Serial.print("Get ");
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.print(j);
+        Serial.println();
+        #endif
+        counter = i * 5 + j;
+        Send_radio(counter);
+        led_blink(counter);
+        delay(100);
+      }
+    }
+    digitalWrite(pinC_button[i], HIGH);
+  }
+}
 
 void loop() {
-  for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[whiteLed] = CRGB(255,255,0);
+  // for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+  //     // Turn our current led on to white, then show the leds
+  //     leds[whiteLed] = CRGB(255,255,0);
 
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();
+  //     // Show the leds (only one of which is set to white, from above)
+  //     FastLED.show();
 
-      // Wait a little bit
-      delay(100);
+  //     // Wait a little bit
+  //     delay(100);
 
-      // Turn our current led back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
-   }
-  //  Serial.print (digitalRead(PIN_R1_BUTTONS));
-  //  Serial.print (" ");
-  //  Serial.print (digitalRead(PIN_R2_BUTTONS));
-  //  Serial.print (" ");
-  //  Serial.print (digitalRead(PIN_R3_BUTTONS));
-  //  Serial.print (" ");
-  //  Serial.print (digitalRead(PIN_R4_BUTTONS));
-  //  Serial.print (" ");
-  //  Serial.print (digitalRead(PIN_R5_BUTTONS));
-  //  Serial.println (" ");
+  //     // Turn our current led back to black for the next loop around
+  //     leds[whiteLed] = CRGB::Black;
+  //  }
+
 //  for (int i = 20; i < 30 ; i++) {
 //    counter = i;
 //    Serial.println (i);
 //    radio.write(&i, sizeof(i));
-    // Check_Button();
+    Check_Button();
 //    delay(100);
 //  }
 }
